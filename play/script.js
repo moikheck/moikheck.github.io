2c9d01903d158b1f46c7ed7434f99a8d2699542d
Michael Heckman <whatthemheckman@gmail.com>
2024-08-26 17:35:17 -0400

    timing update

--- play/script.js
+++ play/script.js
@@ -422,6 +422,8 @@ document.getElementById('start-game').addEventListener('click', function () {
                 gameInfo.preloadedTilesets.push(preload7);
             })
         })
+        console.log(gameInfo.preloadedTilesets);
+        setTimeout(500);
         tileset = gameInfo.preloadedTilesets[Math.floor(Math.random() * gameInfo.preloadedTilesets.length)];
     } else {
     
